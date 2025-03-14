8e8c06f1340d2fb6969ca53d3257e8f498d16518
Alfonso Lorbes <alfonsolorbes04@gmail.com>
2025-03-14 13:30:58 +0100

    quito archivos innecesarios

--- src/app.py
+++ src/app.py
@@ -96,4 +96,4 @@ def deleteUserMONGO():
     return jsonify({'usuarios':"USUARIO ELIMINADO CORRECTAMENTE"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=6000)
+    app.run(debug=True, port=8000, host='0.0.0.0')
